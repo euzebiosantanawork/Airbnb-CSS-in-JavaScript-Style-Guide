faf3c2ff52faf3528de11045a1ede1a25e11a283
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-02-01 17:42:01 -0300

    Create code01.js

---
+++ code01.js
@@ -0,0 +1,19 @@
+/*
+Why? We acess these keys as properties on the style object in the component, so it is most
+conenient to use camelCase
+*/
+
+//bab
+
+{
+'bermuda-trangule':{
+display:'none'
+},
+}
+
+//good
+{
+bermudaTriangle: {
+display: 'none' ,
+ },
+ }
